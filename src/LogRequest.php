6e11b41e542115c4f460f5b10e555815f9513866
Rory Hellyer <rory@webmodularity.com>
2016-12-22 14:05:29 -0800

    dev

--- src/LogRequest.php
+++ src/LogRequest.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class LogRequest extends Model
 {
     public $timestamps = false;
+
+    public function urlPath()
+    {
+        return $this->belongsTo('WebModularity\LaravelLog\LogUrlPath');
+    }
+
+    public function userAgent()
+    {
+        return $this->belongsTo('WebModularity\LaravelLog\LogUserAgent');
+    }
 }
