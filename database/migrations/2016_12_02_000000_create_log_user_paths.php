67684dd02147c15b4cfbe007c4e3772c2054bd0c
Rory Hellyer <rory@webmodularity.com>
2016-12-23 04:11:56 -0800

    Change class names

--- database/migrations/2016_12_02_000000_create_log_user_paths.php
+++ database/migrations/2016_12_02_000000_create_log_user_paths.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLogRequests extends Migration
+class CreateLogUrlPaths extends Migration
 {
     /**
      * Run the migrations.
