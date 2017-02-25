71110aba1c70f36964c270fdb3c349ce49e22b90
Rory Hellyer <rory@webmodularity.com>
2017-02-24 20:29:06 -0800

    add log_query_strings table

--- src/LogUserAgent.php
+++ src/LogUserAgent.php
@@ -23,6 +23,13 @@ class LogUserAgent extends Model
     protected $fillable = ['user_agent', 'user_agent_hash'];
 
     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = ['user_agent_hash'];
+
+    /**
      * Creates a hash from user_agent string
      *
      * @param $userAgent string The user_agent string sent by browser during http request
